1f8ffb83d9c4b008bb281109338fed1bc810774e
AyseKoesekul <85225525+AyseKoesekul@users.noreply.github.com>
2021-06-24 16:49:47 +0200

    js

---
+++ js/code.js
@@ -0,0 +1,19 @@
+$(function(){
+    
+    $('a[href*="#"]').click(function (e) {
+        var href = $(this).attr('href'),
+        $scrollAim = $(href);
+
+        // Abbrechen wenn kein Sprungziel gefunden
+        if ($scrollAim.length == 0) return;
+
+        // Automatisches Scrollen verhindern
+        e.preventDefault();
+
+        // Zum Sprungziel animieren
+        $('html,body').animate({
+            scrollTop: $scrollAim.offset().top
+        }, 700);
+    });
+
+});
